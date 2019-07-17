d415cd75795866a0b3a94a2e6f9518eb95b26306
BuildTools <unconfigured@null.spigotmc.org>
2019-07-17 15:06:09 -0700

    2

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys, pygame, random, pandas as pd
-from download import Ship
-from earth import Astoroid
+from nyancat import Ship
+from earth import Asteroid
 import matplotlib.pyplot as plt
-import numpy as no
+import numpy as np
 from pygame.locals import *
 
 pygame.init()
-screen_info = pygame.displey.Info()
+screen_info = pygame.display.Info()
 
-size = (width, hight) = (int(screen_info.current_w * 0.5), int(screen_current_h * 0.5)
+size = (width, height) = (int(screen_info.current_w * 0.5), int(screen_info.current_h * 0.5))
 
-screen == pygame.displey.set_mode(size)
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 color = (30, 0, 30)
 screen.fill(color)
@@ -21,44 +21,43 @@ earth = pygame.sprite.Group()
 numLevels = df['LevelNum'].max()
 level = df['LevelNum'].min()
 levelData = df.iloc[level]
-asteroidCount = levelData['AstoroidCount']
+asteroidCount = levelData['AsteroidCount']
 player = Ship((levelData['PlayerX'], levelData['PlayerY']))
 tries = 0
 totalTries = []
 
 def init():
-    global asteroidCount, astoroids, levelData, tries
+    global asteroidCount, earth, levelData, tries
     levelData = df.iloc[level]
     player.reset((levelData['PlayerX'], levelData['PlayerY']))
     earth.empty()
-    astoroidCount = levelData['AsteroidCount']
-    for i in range (asteroidcount):
-        earth.add(Asteroid((random.randint(50, width -50), random.randint(50, hight - 50,random.randint(15, 60))))
+    asteroidCount = levelData['AsteroidCount']
+    for i in range (asteroidCount):
+        earth.add(Asteroid((random.randint(50, width -50), random.randint(50, height - 50)),random.randint(15, 60)))
     tries = 1
 
 def win():
-        font = pygame.font,SysFont(None, 70)
-        text = font.render("You Win",True, (0,255,0))
-        text_rect = text.get_rect()
-        text_rect.center = (width/2, hight/2)
+    font = pygame.font.SysFont(None, 70)
+    text = font.render("You Win",True, (0,255,0))
+    text_rect = text.get_rect()
+    text_rect.center = (width/2, height/2)
+    index = np.arange(len(totalTries))
+    plt.bar(index, totalTries)
+    plt.xlabel('Level Number', fontsize = 15)
+    plt.ylabel('Tries', fontsize = 15)
+    plt.title('Tries per level!')
+    plt.show()
 
-        index = np.arrange(len(totalTrise))
-        plt.bar(ndex, totalTries)
-        plt.xlebel('Level Number', fontsize=15)
-        plt.ylabel('Trise', font size=15)
-        plt.tital('Tries per level!')
-        plt.show()
-
-        while True:
-            screen.fill(color)
-            screen.blit(text, text_rect)
-            pygame.dosplay.flip()
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    sys.exit()
+    while True:
+        screen.fill(color)
+        screen.blit(text, text_rect)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
 
 def main():
-    global level, trise,totalTries
+    global level, tries,totalTries
     init()
     while level <= numLevels:
         clock.tick(60)
@@ -83,28 +82,29 @@ def main():
                 if event.key == K_w:
                     player.speed[1] = 0
 
-    screen.fill(color)
-    player.update()
-    earth.update()
-    get_hit = pygame.sprite.spritecollide(player, asteroids, False)
-    earth.draw(screen)
-    screen.blit(player.image, player.rect)
-    pygame.displey.flip()
+        screen.fill(color)
+        player.update()
+        earth.update()
+        gets_hit = pygame.sprite.spritecollide(player, earth, False)
+        earth.draw(screen)
+        screen.blit(player.image, player.rect)
+        pygame.display.flip()
 
-    if player.checkReset(width):
-        totalTrise.appent(tries)
-        if level == numLevels:
-            break
-        else:
-            level +=1
-            init()
-    elif gets_hit:
-        player.reset((levelData['PlayerX'], levelData['playerY']))
-        tries +=1
+        if player.checkReset(width):
+            totalTries.append(tries)
+            if level == numLevels:
+                break
+            else:
+                level +=1
+                init()
+        elif gets_hit:
+            player.reset((levelData['PlayerX'], levelData['PlayerY']))
+            tries +=1
 
     win()
+
 if __name__ == '__main__':
-    main
+    main()
 
 
 
--- nyancat.py
+++ nyancat.py
@@ -2,10 +2,10 @@ import pygame, random
 
 class Ship(pygame.sprite.Sprite):
 
-    def __int__(selfself, pos):
-        super().__int__()
+    def __init__(self, pos):
+        super().__init__()
         self.image = pygame.image.load('download.png')
-        self.image = pygame.transrorm.smoothscale(self.image, (40, 40))
+        self.image = pygame.transform.scale(self.image, (40, 40))
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.speed = pygame.math.Vector2(0, 0)
@@ -13,9 +13,9 @@ class Ship(pygame.sprite.Sprite):
     def update(self):
         self.rect.move_ip(self.speed)
 
-    def checkReset(selfself, endPos):
-        retern self.rect.center[0] > entPos
+    def checkReset(self, endPos):
+        return self.rect.center[0] > endPos
 
-    def reset(selfself, pos):
-        self.rect
+    def reset(self, pos):
+        self.rect.center = pos
 
